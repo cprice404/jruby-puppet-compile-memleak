07078ab359e6b4ebdfee80bcc05056756b7e73c6
Jeremy Barlow <jeremy.barlow@puppetlabs.com>
2014-07-08 11:11:51 -0700

    Changed MYRES in watchpidmem to use ps so will work on OS too

--- watchpidmem.sh
+++ watchpidmem.sh
@@ -3,7 +3,7 @@
 PROCESSNAME=memleak
 MYPID=`jps -m |grep -i memleak |awk '{print $1}'`
 MYTIME=`date +%H:%M:%S`
-MYRES=`echo 0 $(cat /proc/$MYPID/smaps  | grep Rss | awk '{print $2}' | sed 's#^#+#') | bc;`
+MYRES=`ps -o rss $MYPID | awk 'NR==2'`
 MYRES=`echo "scale=2; $MYRES / 1024" |bc -l`
 
 echo "PID:$MYPID TIME:$MYTIME RES:$MYRES"
