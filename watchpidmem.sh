cece4471892fe113161ec577bc5f0699553a95b8
Chris Price <chris@puppetlabs.com>
2014-07-11 16:49:11 -0700

    Improve watchpidmem.sh

--- watchpidmem.sh
+++ watchpidmem.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 PROCESSNAME=memleak
-MYPID=`jps -m |grep -i memleak |awk '{print $1}'`
-MYTIME=`date +%H:%M:%S`
-MYRES=`ps -o rss $MYPID | awk 'NR==2'`
-MYRES=`echo "scale=2; $MYRES / 1024" |bc -l`
+ORIGPID=`jps -m |grep -i memleak |awk '{print $1}'`
+START=`date +%s`
 
-echo "PID:$MYPID TIME:$MYTIME RES:$MYRES"
+while sleep 5;
+do
+   MYPID=`jps -m |grep -i memleak |awk '{print $1}'`
+   if [ "$ORIGPID" != "$MYPID" ]
+   then
+      ORIGPID=$MYPID
+      START=`date +%s`     
+   fi
+   MYTIME=`date +%H:%M:%S`
+   MYRES=`ps -o rss $MYPID | awk 'NR==2'`
+   MYRES=`echo "scale=2; $MYRES / 1024" |bc -l`
+   END=`date +%s`
+   ELAPSED=$((END-START))
+   echo "PID:$MYPID TIME:$MYTIME RES:$MYRES (elapsed: ${ELAPSED}s)";
+done
