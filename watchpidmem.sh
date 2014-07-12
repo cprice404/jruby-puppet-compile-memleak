a33beb7963c8b0933bc01d79a0c7c2b7f143512b
Chris Price <chris@puppetlabs.com>
2014-07-11 17:05:17 -0700

    Another minor improvement to watcher script

--- watchpidmem.sh
+++ watchpidmem.sh
@@ -4,6 +4,11 @@ PROCESSNAME=memleak
 ORIGPID=`jps -m |grep -i memleak |awk '{print $1}'`
 START=`date +%s`
 
+if [ "$ORIGPID" == "" ]
+then
+   echo "Program does not appear to be running yet."
+fi
+
 while sleep 5;
 do
    MYPID=`jps -m |grep -i memleak |awk '{print $1}'`
@@ -11,11 +16,18 @@ do
    then
       ORIGPID=$MYPID
       START=`date +%s`     
+      if [ "$MYPID" == "" ]
+      then
+         echo "Program appears to have terminated."
+      fi
    fi
-   MYTIME=`date +%H:%M:%S`
-   MYRES=`ps -o rss $MYPID | awk 'NR==2'`
-   MYRES=`echo "scale=2; $MYRES / 1024" |bc -l`
-   END=`date +%s`
-   ELAPSED=$((END-START))
-   echo "PID:$MYPID TIME:$MYTIME RES:$MYRES (elapsed: ${ELAPSED}s)";
+   if [ "$MYPID" != "" ]
+   then
+      MYTIME=`date +%H:%M:%S`
+      MYRES=`ps -o rss $MYPID | awk 'NR==2'`
+      MYRES=`echo "scale=2; $MYRES / 1024" |bc -l`
+      END=`date +%s`
+      ELAPSED=$((END-START))
+      echo "PID:$MYPID TIME:$MYTIME RES:$MYRES (elapsed: ${ELAPSED}s)";
+   fi
 done
