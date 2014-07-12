92d9d906a89b8d1b9173e1bd1e43d5bde67f32b1
Chris Price <chris@puppetlabs.com>
2014-07-12 16:48:54 -0700

    Lower heap to 64, a few more tweaks to watcher script

--- watchpidmem.sh
+++ watchpidmem.sh
@@ -8,24 +8,22 @@ fi
 echo "Delay set to $DELAY"
 
 PROCESSNAME=memleak
-ORIGPID=`jps -m |grep -i memleak |awk '{print $1}'`
-START=`date +%s`
-
-if [ "$ORIGPID" == "" ]
-then
-   echo "Program does not appear to be running yet."
-fi
+ORIGPID="firstpid"
 
 while sleep $DELAY;
 do
-   MYPID=`jps -m |grep -i memleak |awk '{print $1}'`
+   MYPID=`jps -m |grep -i $PROCESSNAME |awk '{print $1}'`
    if [ "$ORIGPID" != "$MYPID" ]
    then
       ORIGPID=$MYPID
       START=`date +%s`     
       if [ "$MYPID" == "" ]
       then
-         echo "Program appears to have terminated."
+         echo "Program does not appear to be running"
+      else
+         GITSHA=`git rev-parse --short HEAD`
+         echo "Program started; git SHA: $GITSHA"
+         echo "PID  	TIME    	RES   	ELAPSED";
       fi
    fi
    if [ "$MYPID" != "" ]
@@ -35,6 +33,7 @@ do
       MYRES=`echo "scale=2; $MYRES / 1024" |bc -l`
       END=`date +%s`
       ELAPSED=$((END-START))
-      echo "PID:$MYPID TIME:$MYTIME RES:$MYRES (elapsed: ${ELAPSED}s)";
+      #echo "PID:$MYPID TIME:$MYTIME RES:$MYRES (elapsed: ${ELAPSED}s)";
+      echo "$MYPID	$MYTIME	$MYRES	$ELAPSED";
    fi
 done
